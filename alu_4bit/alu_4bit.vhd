46cf2d4da2237c5098b236e61d3af1ee0d5da442
Aarya Vedpathak <165999849+AaryaVedpathak@users.noreply.github.com>
2025-06-22 14:00:08 +0530

    Update alu_4bit.vhd

--- alu_4bit/alu_4bit.vhd
+++ alu_4bit/alu_4bit.vhd
@@ -42,33 +42,33 @@ end alu_4bit;
 
 architecture Behavioral of alu_4bit is
 begin
-process(n1,n2,op)
-variable temp : std_logic_vector(4 downto 0);
-begin
-flag<='0';
-case op is 
-when "000"=>
-temp :=('0' &n1)+n2;
-res <=temp(3 downto 0);
-flag <=temp(4);
-when"001" =>
-if(n1>n2) then
-res <= n1 - n2;
-else 
-res <= n2 - n1;
-end if;
-when "010" =>
-res <= n1 nand n2;
-when "011" =>
-res <= n2 nor n2;
-when "100" =>
-res <= not n2;
-when "101" =>
-res <= n1 or n2;
-when "110" =>
-res <= n1 xor n2;
-when others =>
-res <= n1;
-end case;
-end process;
+    process(n1,n2,op)
+    variable temp : std_logic_vector(4 downto 0);
+    begin
+        flag<='0';
+        case op is 
+            when "000"=>
+                temp :=('0' &n1)+n2;
+                res <=temp(3 downto 0);
+                flag <=temp(4);
+            when"001" =>
+                if(n1>n2) then
+                    res <= n1 - n2;
+                else 
+                    res <= n2 - n1;
+                end if;
+            when "010" =>
+                res <= n1 nand n2;
+            when "011" =>
+                res <= n2 nor n2;
+            when "100" =>
+                res <= not n2;
+            when "101" =>
+                res <= n1 or n2;
+            when "110" =>
+                res <= n1 xor n2;
+            when others =>
+                res <= n1;
+        end case;
+    end process;
 end Behavioral;
